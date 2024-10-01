50af7e47d77e32b135b323749cb266b3a5b96c5c
Meacronacer <sinnerscrown09@gmail.com>
2024-10-01 06:47:19 +0300

    chat layout

--- frontend/src/app/pages/chat-page/chat-page.component.ts
+++ frontend/src/app/pages/chat-page/chat-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../../shared/components/ui/svg-icon/svg-icon.component';
+import { ChatUserComponent } from "./chat-user/chat-user.component";
+import { ProfileInputComponent } from "../profile-page/profile-input/profile-input.component";
 
 @Component({
   selector: 'app-chat-page',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent, ChatUserComponent, ProfileInputComponent],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.scss'
 })
